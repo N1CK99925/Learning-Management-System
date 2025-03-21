673c38cbd4688c62b5c1ca8cd9264f39df272853
itsMeN1CK <68419702+itsMeN1CK@users.noreply.github.com>
2025-03-21 14:49:41 +0530

    removed bs

--- src/main/java/com/example/ProductManagement/users/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ProductManagement.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findByRole(String role); // Fetch users by role
-}
-
