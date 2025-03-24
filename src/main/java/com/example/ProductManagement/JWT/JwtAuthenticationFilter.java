9c47c243d8d8f5b97539ac4b583d6eaaf5634467
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2025-03-24 19:33:21 +0530

    Update JwtAuthenticationFilter.java

--- src/main/java/com/example/ProductManagement/JWT/JwtAuthenticationFilter.java
+++ src/main/java/com/example/ProductManagement/JWT/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.example.ProductManagement.JWT;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import org.springframework.lang.NonNull;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,7 +24,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         String authHeader = request.getHeader("Authorization");
