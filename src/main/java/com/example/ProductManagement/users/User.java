4c8a4fe02c1ef2dfa0566611574ddefdcc5ac812
itsMeN1CK <68419702+itsMeN1CK@users.noreply.github.com>
2025-03-26 09:46:31 +0530

    BuilderDefault

--- src/main/java/com/example/ProductManagement/users/User.java
+++ src/main/java/com/example/ProductManagement/users/User.java
@@ -28,7 +28,7 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
-
+    @Builder.Default
     @Column(nullable = true)
     private Boolean enabled = true;
 }
